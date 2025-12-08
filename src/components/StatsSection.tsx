c7033dff1c24620ade49d3fd2b1896a8f5baa20b
Ayush Gupta <95233360+ayu-g22@users.noreply.github.com>
2025-12-08 14:55:39 +0530

    Responsive web - v1

--- src/components/StatsSection.tsx
+++ src/components/StatsSection.tsx
@@ -9,27 +9,23 @@ export const StatsSection = () => {
   return (
     <section id="red-bg" className="relative w-full bg-red py-12 sm:py-16 md:py-20 lg:py-24">
 
+      <div className="relative max-w-7xl mx-auto px-4 sm:px-6 md:px-8 lg:px-12 
+      grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 sm:gap-10 md:gap-12 lg:gap-16 text-center">
 
-      <div className="relative max-w-7xl mx-auto px-4 sm:px-6 md:px-8 lg:px-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 sm:gap-10 md:gap-12 lg:gap-16 text-center">
-
-        {/* 1 - Pilots */}
+        {/* 1 - Professional Pilots */}
         <div className="flex flex-col items-center">
 
           <h3 className="font-montserrat font-bold text-4xl sm:text-5xl md:text-6xl text-white">
             89<span className="text-gold text-2xl sm:text-3xl md:text-4xl">+</span>
           </h3>
 
-          <p className="font-montserrat text-white/80 text-lg">
+          <LiaCertificateSolid
+            size={65}
+            className="text-gold pb-2 sm:w-[75px] sm:h-[75px] md:w-[85px] md:h-[85px]"
+          />
+
+          <p className="font-montserrat text-white/80 text-lg mt-2">
             Professional Pilots
-          <LiaCertificateSolid size={65} className="text-gold pb-2 sm:w-[75px] sm:h-[75px] md:w-[85px] md:h-[85px]" />
-
-          <h3 className="font-montserrat font-bold text-4xl sm:text-5xl md:text-6xl text-white">
-            89<span className="text-gold text-2xl sm:text-3xl md:text-4xl">+</span>
-          </h3>
-
-          <p className="font-montserrat py-3 sm:py-4 md:py-5 text-white/80 text-xl sm:text-2xl md:text-3xl font-bold">
-            Pilots
-
           </p>
 
           <div className="h-[2px] sm:h-[3px] w-32 sm:w-36 md:w-40 bg-gold mt-4 sm:mt-5 md:mt-6" />
@@ -42,15 +38,12 @@ export const StatsSection = () => {
             289<span className="text-gold text-2xl sm:text-3xl md:text-4xl">+</span>
           </h3>
 
-          <p className="font-montserrat text-white/80 text-lg">
-          <LiaGlobeSolid size={65} className="text-gold2 pb-2 sm:w-[75px] sm:h-[75px] md:w-[85px] md:h-[85px]" />
+          <LiaGlobeSolid
+            size={65}
+            className="text-gold2 pb-2 sm:w-[75px] sm:h-[75px] md:w-[85px] md:h-[85px]"
+          />
 
-          <h3 className="font-montserrat font-semibold text-4xl sm:text-5xl md:text-6xl text-white">
-            289<span className="text-gold text-2xl sm:text-3xl md:text-4xl">+</span>
-          </h3>
-
-          <p className="font-montserrat py-3 sm:py-4 md:py-5 text-white/80 text-xl sm:text-2xl md:text-3xl font-bold">
-
+          <p className="font-montserrat text-white/80 text-lg mt-2">
             World Airports
           </p>
 
@@ -64,15 +57,12 @@ export const StatsSection = () => {
             59<span className="text-gold text-2xl sm:text-3xl md:text-4xl">+</span>
           </h3>
 
-          <p className="font-montserrat text-white/80 text-lg">
-          <LiaPaperPlaneSolid size={65} className="text-gold2 pb-2 sm:w-[75px] sm:h-[75px] md:w-[85px] md:h-[85px]" />
+          <LiaPaperPlaneSolid
+            size={65}
+            className="text-gold2 pb-2 sm:w-[75px] sm:h-[75px] md:w-[85px] md:h-[85px]"
+          />
 
-          <h3 className="font-montserrat font-semibold text-4xl sm:text-5xl md:text-6xl text-white">
-            59<span className="text-gold text-2xl sm:text-3xl md:text-4xl">+</span>
-          </h3>
-
-          <p className="font-montserrat py-3 sm:py-4 md:py-5 text-white/80 text-xl sm:text-2xl md:text-3xl font-bold">
-
+          <p className="font-montserrat text-white/80 text-lg mt-2">
             Jet Airplanes
           </p>
 
@@ -86,15 +76,12 @@ export const StatsSection = () => {
             129<span className="text-gold text-2xl sm:text-3xl md:text-4xl">+</span>
           </h3>
 
-          <p className="font-montserrat text-white/80 text-lg">
-          <LiaMapSolid size={65} className="text-gold2 pb-2 sm:w-[75px] sm:h-[75px] md:w-[85px] md:h-[85px]" />
+          <LiaMapSolid
+            size={65}
+            className="text-gold2 pb-2 sm:w-[75px] sm:h-[75px] md:w-[85px] md:h-[85px]"
+          />
 
-          <h3 className="font-montserrat font-semibold text-4xl sm:text-5xl md:text-6xl text-white">
-            129<span className="text-gold text-2xl sm:text-3xl md:text-4xl">+</span>
-          </h3>
-
-          <p className="font-montserrat py-3 sm:py-4 md:py-5 text-white/80 text-xl sm:text-2xl md:text-3xl font-bold">
-
+          <p className="font-montserrat text-white/80 text-lg mt-2">
             Directions
           </p>
 
